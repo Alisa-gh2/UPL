55ce0ec0bf75f9d605f6456d51e37a10cfd1e2df
Alisa-gh2 <154425195+Alisa-gh2@users.noreply.github.com>
2025-04-16 06:50:53 +0300

    Add files via upload

---
+++ upl2.2/upl2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое слово");
+        string word1 = Console.ReadLine();
+        Console.WriteLine("Введите второе слово");
+        string word2 = Console.ReadLine();
+        string content = File.ReadAllText("C:\\.ВРОДЕ ЛОКАЛЬНЫЙ ДИСК Х\\Учеба 1 курс\\Учебно-лабораторный практикум\\randomtext.txt");
+        content = content.Replace(word1, word2);
+        File.WriteAllText("C:\\.ВРОДЕ ЛОКАЛЬНЫЙ ДИСК Х\\Учеба 1 курс\\Учебно-лабораторный практикум\\randomtext.txt", content);
+        Console.WriteLine("Замена выполнена");
+    }
+}
